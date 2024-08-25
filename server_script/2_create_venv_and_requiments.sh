e4433e054993b73148d305dfbf7ef133fb6b7223
Sanjar Arani Ali <letsmailsanjar@gmail.com>
2024-08-25 16:06:10 +0330

    add create venv script and downlaod datasets script

---
+++ server_script/2_create_venv_and_requiments.sh
@@ -0,0 +1,5 @@
+python3.8 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+pip install Pillow==9.5.0
+pip install lmdb
---
+++ server_script/3_download_move_datasets.sh
@@ -0,0 +1,3 @@
+mkdir ../datasets/ffhq256.lmdb
+gdown "https://drive.google.com/uc?id=1ZPs9e5_RxZ-JL36udRzWsULv0mpo_Hhv" -O eeg_5_95_std.pth
+mv eeg_5_95_std.pth ../datasets
