2fc9e1e557ad1f7311802e9ef278eea6f8567412
Sanjar Arani Ali <letsmailsanjar@gmail.com>
2024-08-26 20:18:55 +0330

    reduce number of parallel download in pip

--- server_script/2_create_venv_and_requiments.sh
+++ server_script/2_create_venv_and_requiments.sh
@@ -6,8 +6,8 @@ source venv/bin/activate
 #pip install torch==1.8.1+cu111 torchvision==0.9.1+cu111 torchaudio==0.8.1 -f https://download.pytorch.org/whl/torch_stable.html
 #pip install -r requirements_server.txt
 #pip install Pillow==9.5.0
-cat requirements_server.txt | xargs -t -L 1 -P9 pip download -q
-cat requirements_server.txt | xargs -t -L 1 -P9 pip install -q
+cat requirements_server.txt | xargs -t -L 1 -P3 pip download -q
+cat requirements_server.txt | xargs -t -L 1 -P1 pip install -q
 
 cd $CURRENT_PATH
 pip install gdown
