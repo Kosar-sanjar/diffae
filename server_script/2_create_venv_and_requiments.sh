72abcbb58360cea3b9ecefb68437daaf49919093
Sanjar Arani Ali <letsmailsanjar@gmail.com>
2024-08-26 20:10:32 +0330

    update server requirments

--- server_script/2_create_venv_and_requiments.sh
+++ server_script/2_create_venv_and_requiments.sh
@@ -1,6 +1,13 @@
+CURRENT_PATH=$(pwd)
+cd ..
+
 python3.8 -m venv venv
 source venv/bin/activate
-pip install torch==1.8.1+cu111 torchvision==0.9.1+cu111 torchaudio==0.8.1 -f https://download.pytorch.org/whl/torch_stable.html
-pip install -r requirements_server.txt
-pip install Pillow==9.5.0
+#pip install torch==1.8.1+cu111 torchvision==0.9.1+cu111 torchaudio==0.8.1 -f https://download.pytorch.org/whl/torch_stable.html
+#pip install -r requirements_server.txt
+#pip install Pillow==9.5.0
+cat requirements_server.txt | xargs -t -L 1 -P9 pip download -q
+cat requirements_server.txt | xargs -t -L 1 -P9 pip install -q
+
+cd $CURRENT_PATH
 pip install gdown
