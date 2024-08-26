d240453aaaedb6bc0c14fd54b131624778334446
Sanjar Arani Ali <letsmailsanjar@gmail.com>
2024-08-26 22:49:23 +0330

    add download of split file

--- server_script/3_download_move_datasets.sh
+++ server_script/3_download_move_datasets.sh
@@ -5,7 +5,11 @@ source venv/bin/activate
 pip install gdown
 
 mkdir datasets/ffhq256.lmdb
+
 gdown "https://drive.google.com/uc?id=1ZPs9e5_RxZ-JL36udRzWsULv0mpo_Hhv" -O eeg_5_95_std.pth
 mv eeg_5_95_std.pth datasets
+
+gdown "https://drive.google.com/uc\?id\=1ZQaX_-jtW8nwRd6PiPv_UN0nI9GhgwEq" -O block_splits_by_image_all.pth
+mv block_splits_by_image_all.pth datasets
 
 cd $CURRENT_PATH
