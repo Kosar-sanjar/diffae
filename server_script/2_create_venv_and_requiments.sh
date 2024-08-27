8da108dd0c6635c4bc8a0c81adc7338a4c93e07b
Sanjar Arani Ali <letsmailsanjar@gmail.com>
2024-08-27 15:06:03 +0330

    bug fix:update python requirment install script and remove parallel downlaod

--- server_script/2_create_venv_and_requiments.sh
+++ server_script/2_create_venv_and_requiments.sh
@@ -3,10 +3,7 @@ cd ..
 
 python3.8 -m venv venv
 source venv/bin/activate
-#pip install torch==1.8.1+cu111 torchvision==0.9.1+cu111 torchaudio==0.8.1 -f https://download.pytorch.org/whl/torch_stable.html
-#pip install -r requirements_server.txt
-#pip install Pillow==9.5.0
-cat requirements_server.txt | xargs -t -L 1 -P3 pip download -q
-cat requirements_server.txt | xargs -t -L 1 -P1 pip install -q
+pip install torch==1.8.1+cu111 torchvision==0.9.1+cu111 torchaudio==0.8.1 -f https://download.pytorch.org/whl/torch_stable.html
+pip install -r requirements_server.txt
 
 cd $CURRENT_PATH
