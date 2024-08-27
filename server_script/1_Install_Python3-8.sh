e3affb08d7c524f3b778878b3b85e1aa9001b088
Sanjar Arani Ali <letsmailsanjar@gmail.com>
2024-08-27 13:32:26 +0330

    add Image dataset download link

--- server_script/1_Install_Python3-8.sh
+++ server_script/1_Install_Python3-8.sh
@@ -9,7 +9,7 @@ sudo apt-get -y install apt-fast
 sudo apt-fast install -y build-essential libssl-dev zlib1g-dev libbz2-dev \
                         libreadline-dev libsqlite3-dev wget curl llvm \
                         libncurses5-dev libncursesw5-dev xz-utils tk-dev \
-                        libffi-dev liblzma-dev python3-openssl git libbz2-dev
+                        libffi-dev liblzma-dev python3-openssl git libbz2-dev unzip
 
 mkdir python38
 cd python38
--- server_script/3_download_move_datasets.sh
+++ server_script/3_download_move_datasets.sh
@@ -12,4 +12,8 @@ mv eeg_5_95_std.pth datasets
 gdown "https://drive.google.com/uc\?id\=1ZQaX_-jtW8nwRd6PiPv_UN0nI9GhgwEq" -O block_splits_by_image_all.pth
 mv block_splits_by_image_all.pth datasets
 
+gdown https://drive.google.com/uc\?id\=1fc_wDKQul6PakR2Dtg4sKeUhVaoLM6xz -O images.zip 
+mv images.zip datasets
+unzip datasets/images.zip -d datasets/images 
+
 cd $CURRENT_PATH
